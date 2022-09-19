e0d972db69b85994e198efb7ef5e420633897e71
Slava Lampe <lvd1970@gmail.com>
2022-09-19 13:23:13 +0300

    add Function

--- main.py
+++ main.py
@@ -3,3 +3,14 @@ print("Artem")
 print("hello python")
 print("qwerty")
 print("vork")
+
+def Fun(a, b):
+    c = b
+    b = a
+    a = c
+    return(a, b)
+
+
+q = Fun(input("Первый аргумент:"), input("Второй аргумент:"))
+print("Первый аргумент:", q[0])
+print("Второй аргумент:", q[1])
